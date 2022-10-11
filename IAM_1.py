3da578a8b1e63af5743da0d476a5dc51006775fa
NiRovil <nicolas199851@hotmail.com>
2022-10-11 09:49:58 -0300

    aws iam consult function

---
+++ IAM_1.py
@@ -0,0 +1,38 @@
+import json
+from boto3 import Session
+from datetime import datetime
+
+ACCESS_KEY = ''
+SECRET_KEY = ''
+
+session = Session(aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+
+client = session.client('iam')
+users = client.list_users()
+
+parse = json.dumps(users, indent=4, sort_keys=True, default=str)
+response = json.loads(parse)
+parse_dict = dict(response)
+
+flagged_users = []
+
+for item in parse_dict['Users']:
+    flagged_user = {}
+    for key in item:
+        if key == 'PasswordLastUsed':
+            last_used = item['PasswordLastUsed'][:19]
+            last_used_date = datetime.strptime(last_used, '%Y-%m-%d %H:%M:%S')
+            last_activity = datetime.now(last_used_date.tzinfo) - last_used_date
+
+            if last_activity.days >= 55:
+                flagged_user['username'] = item['UserName']
+                flagged_user['last_activity'] = last_activity.days
+
+                flagged_users.append(flagged_user)
+            else:
+                continue
+
+with open('lista_usuarios.xls', 'w') as file:
+    for user in flagged_users:
+        file.writelines("Usuario: %s\n" % user['username'])
+        file.writelines("Dias sem uso: %s\n" % user['last_activity'])
