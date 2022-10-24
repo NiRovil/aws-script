8b1a505178da211f6986cc8c9b88772fc73bbfde
NiRovil <nicolas199851@hotmail.com>
2022-10-24 16:10:12 -0300

    ajustando para funcionamento em aws lambda

--- ec2_sec_groups.py
+++ ec2_sec_groups.py
@@ -2,6 +2,7 @@
     Esse script tem como objetivo listar todos os Security Groups, filtrando somente
     os IP Permissions.
 """
+
 from discord_webhook import DiscordWebhook
 from boto3 import Session
 
@@ -11,37 +12,48 @@ URL_BOT = ''
 PORTS = []
 
 session = Session(aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
-webhook = DiscordWebhook(url=URL_BOT, content=f'Portas verificadas: {[port for port in PORTS]}')
 
 client = session.client('ec2', 'us-east-1')
 response = client.describe_security_groups()
 
-with open('sec_groups.txt', 'w') as file:
-    for port in PORTS:
-        for sec_groups in response['SecurityGroups']:
-            for ip_perm in sec_groups['IpPermissions']:
-                if 'FromPort' in ip_perm.keys():
-                    if ip_perm['FromPort'] == port:
-                        file.write(f'{sec_groups["GroupName"]}\n')
+insecure_from_ports = []
+insecure_to_ports = []
+insecure_egress_ports = []
 
+def message(title, text):
+    message = DiscordWebhook(url=URL_BOT, content=f'{title}: {text}')
+    return message
 
-                # Caso haja a necessidade de listar as portas TO:
+# Listando os ingress
+for port in PORTS:
+    for sec_groups in response['SecurityGroups']:
+        for ip_perm in sec_groups['IpPermissions']:
+            if 'FromPort' in ip_perm.keys() and ip_perm['FromPort'] == port:
+                insecure_from_ports.append(sec_groups['GroupName'])
 
-                #if 'ToPort' in ip_perm.keys():
-                    #if ip_perm['ToPort'] == port:
-                        #file.write(f'{sec_groups["GroupName"]}\n')
+            if 'ToPort' in ip_perm.keys() and ip_perm['ToPort'] == port:
+                insecure_to_ports.append(sec_groups['GroupName'])
 
-# Caso haja a necessidade de listar a portas EGRESS
+# Listando os egress
+for port in PORTS:
+    for sec_groups in response['SecurityGroups']:
+        for ip_perm in sec_groups['IpPermissionsEgress']:
+            if 'FromPort' in ip_perm.keys() and ip_perm['FromPort'] == port:
+                insecure_egress_ports.append(sec_groups['GroupName'])
+            
+            if 'ToPort' in ip_perm.keys() and ip_perm['ToPort'] == port:
+                insecure_egress_ports.append(sec_groups['GroupName'])
 
-#with open('sec_groups_egress.txt', 'w') as file:
-    #for sec_groups in response['SecurityGroups']:
-        #for ip_perm in sec_groups['IpPermissionsEgress']:
-            #file.write(f'{sec_groups["GroupName"]} - {sec_groups["IpPermissionsEgress"]}\n')
-
-with open('sec_groups.txt', 'rb') as f:
-    webhook.add_file(file=f.read(), filename='sec_groups.txt')
-
-#with open('sec_groups_egress.txt', 'rb') as f:
-    #webhook.add_file(file=f.read(), filename='sec_groups_egress.txt')
-
-webhook.execute()
+# Definindo as mensagens a serem enviadas via discord webhook
+    if insecure_from_ports or insecure_to_ports:
+        message('Portas ingress FROM verificadas', f'{[port for port in PORTS]} / GroupNames identificados: {[gname for gname in insecure_from_ports]}').execute()
+        message('Portas ingress TO verificadas', f'{[port for port in PORTS]} / GroupNames identificados: {[gname for gname in insecure_to_ports]}').execute()
+        
+    else:
+        message('Portas ingress verificadas', f'{[port for port in PORTS]} / Nenhuma porta insegura identificada' ).execute()
+    
+    if insecure_egress_ports:
+        message('Portas egress FROM/TO verificadas', f'{[port for port in PORTS]} / GroupNames identificados {[gname for gname in insecure_egress_ports]}').execute()
+    
+    else:
+        message('Portas egress verificadas', f'{[port for port in PORTS]} / Nenhuma porta insegura identificada').execute()
